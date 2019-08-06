69f6971e6b041c9127378367ff32a8cd328c4d39
alvinlo92 <alvinlo92@yahoo.com>
2019-08-05 17:33:32 -0700

    Saved current state counter to hard drive

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,14 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, id) => {
+    id += 1;
+    writeCounter(id, (err, id) => {
+      callback(err, id);
+    })
+  });
+  // return undefined;
 };
 
 
